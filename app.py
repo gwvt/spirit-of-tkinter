5a69b6787941950d7cf76e03271d5a23c673456f
Gregory White <greg@gwvt.me>
2017-08-23 13:41:01 -0400

    add comments

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ import tkinter as tk
 
 app = tk.Tk()
 
+# create canvas with dimensions
 canvas = tk.Canvas(app, width=400, height=400)
 canvas.grid()
 
+# call methods on canvas object to draw shape
+# pass coordinates x0, y0, x1, y1 of rectangle bounding the oval
 circle = canvas.create_oval(50, 50, 350, 350)
 
+# call itemconfigure method on canvas object to configure the shape
 canvas.itemconfigure(
     circle,
     fill='yellow',
