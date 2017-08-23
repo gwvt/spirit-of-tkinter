6bd4aaf68c67bb58e650844440c10267122e5458
Gregory White <greg@gwvt.me>
2017-08-23 12:35:10 -0400

    3 create branch add comments

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ def print_it():
     print('The world is, the world is -- love and life are deep.')
 
 
+# create a frame with app as parent
 frame = tk.Frame(app)
 
-
+# pass in options with configure method
 frame.configure(bd=10, relief='raised')
 frame.grid()
 
-
+# create button with frame as parent
 button = tk.Button(
     frame, text='Sing it', command=print_it)
 button.grid()
