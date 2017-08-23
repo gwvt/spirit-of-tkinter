aa23ce6a72dab7767381e196c4828fbe59e07a5d
Gregory White <greg@gwvt.me>
2017-08-23 13:30:04 -0400

    5 code

--- app.py
+++ app.py
@@ -3,18 +3,42 @@ import tkinter as tk
 app = tk.Tk()
 
 
-def make_it_blue(widget):
-    widget.configure(bg='blue')
+# define classes to create multiple frames and buttons
+class ButtonFrame(tk.Frame):
+    def __init__(self, parent):
+        # create instance of tk.Frame as self
+        super().__init__(parent)
+        self.configure(bd=10, relief='raised')
 
 
-frame = tk.Frame(app)
+class BlueButton(tk.Button):
+    # create button instance with parent and text
+    def __init__(self, parent, text):
+        super().__init__(parent)
 
-frame.configure(bd=10, relief='raised')
-frame.grid()
+        # define plain function, not instance method, to pass to lambda
+        def make_parent_blue():
+            # get the widget's parent to configure
+            parent = self.master
+            parent.configure(bg='blue')
 
-# pass lambda to command option with frame as parameter
-button = tk.Button(
-    frame, text='Sing it', command=lambda: make_it_blue(frame))
-button.grid()
+        # configure widget with options passed to __init__ function
+        self.configure(text=text)
+
+        # and common values for all instances
+        self.configure(command=make_parent_blue, fg='red')
+
+
+frame1 = ButtonFrame(app)
+frame1.grid(row=0, column=0)
+
+frame2 = ButtonFrame(app)
+frame2.grid(row=0, column=1)
+
+button1 = BlueButton(frame1, 'Make my parent blue')
+button1.grid()
+
+button2 = BlueButton(frame2, 'No, make my parent blue')
+button2.grid()
 
 app.mainloop()
