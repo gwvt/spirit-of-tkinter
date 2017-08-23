30065bc8811a764c8c30222ad21ea5cb389ebcde
Gregory White <greg@gwvt.me>
2017-08-23 13:40:19 -0400

    6 code

--- app.py
+++ app.py
@@ -2,44 +2,15 @@ import tkinter as tk
 
 app = tk.Tk()
 
+canvas = tk.Canvas(app, width=400, height=400)
+canvas.grid()
 
-# define classes to create multiple frames and buttons
-class ButtonFrame(tk.Frame):
-    def __init__(self, parent):
-        # create instance of tk.Frame as self
-        super().__init__(parent)
-        self.configure(bd=10, relief='raised')
+circle = canvas.create_oval(50, 50, 350, 350)
 
-
-class BlueButton(tk.Button):
-    # create button instance with parent and text
-    def __init__(self, parent, text):
-        super().__init__(parent)
-
-        # define plain function, not instance method, to pass to lambda
-        def make_parent_blue():
-            # get the widget's parent to configure
-            parent = self.master
-            parent.configure(bg='blue')
-
-        # configure widget with options passed to __init__ function
-        self.configure(text=text)
-
-        # and common values for all instances
-        self.configure(command=make_parent_blue, fg='red')
-
-
-frame1 = ButtonFrame(app)
-# place widget at specific location in grid
-frame1.grid(row=0, column=0)
-
-frame2 = ButtonFrame(app)
-frame2.grid(row=0, column=1)
-
-button1 = BlueButton(frame1, 'Make my parent blue')
-button1.grid()
-
-button2 = BlueButton(frame2, 'No, make my parent blue')
-button2.grid()
+canvas.itemconfigure(
+    circle,
+    fill='yellow',
+    outline='black'
+)
 
 app.mainloop()
