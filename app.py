bf2943a2a1200ec71625d7646831f76311535bce
Gregory White <greg@gwvt.me>
2017-08-23 11:43:48 -0400

    2 code

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 
-# create root window
 app = tk.Tk()
 
-# add button widget with text
-button = tk.Button(app, text='A Button')
 
-# place button in the grid of its parent, the root window
+def print_it():
+    print('And the men who hold high places must be the ones to start.')
+
+
+button = tk.Button(app, text='Sing it', command=print_it)
+
 button.grid()
 
 app.mainloop()
