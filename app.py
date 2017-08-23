38322ba9cfac7e919d34a132eb74cec74378b38e
Gregory White <greg@gwvt.me>
2017-08-23 11:46:52 -0400

    add comments

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ def print_it():
     print('And the men who hold high places must be the ones to start.')
 
 
+# pass function to button with command option
 button = tk.Button(app, text='Sing it', command=print_it)
 
 button.grid()
