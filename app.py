ba6671dea3dad2a455668f8ede071cd7ae4c9cb4
Gregory White <greg@gwvt.me>
2017-08-23 11:41:01 -0400

    add comments

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import tkinter as tk
 
+# create root window
 app = tk.Tk()
 
+# add button widget with text
 button = tk.Button(app, text='A Button')
+
+# place button in the grid of its parent, the root window
 button.grid()
 
 app.mainloop()
