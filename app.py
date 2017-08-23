e52c8b58707ca11dd12df0f9f8ee36ea8d8bb0ec
Gregory White <greg@gwvt.me>
2017-08-23 12:43:27 -0400

    change

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import tkinter as tk
 app = tk.Tk()
 
 # add button widget with text
+# give it app as parent with first argument
 # return widget from constructor
 button = tk.Button(app, text='A Button')
 
-# place button widget in the grid of its parent, the root window
+# place button widget in the grid of its parent
 button.grid()
 
 # start main event loop
