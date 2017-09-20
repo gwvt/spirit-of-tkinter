dc8a0a3e8c3134c1415b4956242e5e965d04ee65
Gregory White <greg@gwvt.me>
2017-09-20 10:03:35 -0400

    06 app change comments

--- app.py
+++ app.py
@@ -2,16 +2,17 @@ import tkinter as tk
 
 app = tk.Tk()
 
-# create canvas with dimensions
+# create canvas widget with dimensions
 canvas = tk.Canvas(app, width=400, height=400)
 canvas.grid()
 
-# call methods on canvas object to draw shape
+# call methods on canvas widget to draw shape object
 # pass coordinates x0, y0, x1, y1 of rectangle bounding the oval
 # no parent because the method is not creating a new widget
 circle = canvas.create_oval(50, 50, 350, 350)
 
-# call itemconfigure method on canvas object to configure the shape
+# call itemconfigure method on canvas widget to configure the shape object
+# passed as first argument
 canvas.itemconfigure(
     circle,
     fill='yellow',
