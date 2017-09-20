542220aa8c4713c5ddfcc114a2ef00505df09d24
Gregory White <greg@gwvt.me>
2017-09-20 09:43:35 -0400

    05 app change comments

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tkinter as tk
 app = tk.Tk()
 
 
-# define classes to create multiple frames and buttons
+# define class to create multiple frames
 class ButtonFrame(tk.Frame):
     def __init__(self, parent):
         # create instance of tk.Frame as self
@@ -11,24 +11,27 @@ class ButtonFrame(tk.Frame):
         self.configure(bd=10, relief='raised')
 
 
+# define class to create multiple buttons
 class BlueButton(tk.Button):
     # create button instance with parent and text
     def __init__(self, parent, text):
         super().__init__(parent)
 
-        # define plain function, not instance method, to pass to lambda
+        # define plain function, not instance method, to pass to command option
         def make_parent_blue():
-            # get the widget's parent to configure
+            # get the widget's parent to call its configure method
             parent = self.master
             parent.configure(bg='blue')
 
-        # configure widget with options passed to __init__ function
+        # configure created button widget with options passed
+        # to __init__ function
         self.configure(text=text)
-
-        # and static values for all instances
+        # and with static values for all instances
         self.configure(command=make_parent_blue, fg='red')
 
 
+# now use our custom classes instead of the native tkinter classes
+# for creating frames and buttons
 frame1 = ButtonFrame(app)
 # place widget at specific location in grid
 frame1.grid(row=0, column=0)
@@ -36,6 +39,7 @@ frame1.grid(row=0, column=0)
 frame2 = ButtonFrame(app)
 frame2.grid(row=0, column=1)
 
+# create buttons with display text
 button1 = BlueButton(frame1, 'Make my parent blue')
 button1.grid()
 
