f6ea2b0e0b9912e1a5e6c8befa948526409f2f0f
Gregory White <greg@gwvt.me>
2017-09-20 16:28:07 -0400

    12 app fix typo

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from frames import (ColorsFrame, QuitFrame, PicturesFrame, ShapesFrame,
                     LyricsFrame, CrackleFrame)
 
 
-
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
