17965fe949001b083ee65aae02e4b47a987449c8
Gregory White <greg@gwvt.me>
2017-09-20 22:09:36 -0400

    04 app use partial

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from functools import partial
 
 app = tk.Tk()
 
@@ -14,7 +15,7 @@ frame.grid()
 
 # pass lambda to command option with frame as argument
 button = tk.Button(
-    frame, text='Make me blue', command=lambda: make_it_blue(frame))
+    frame, text='Make me blue', command=partial(make_it_blue, frame))
 button.grid()
 
 app.mainloop()
